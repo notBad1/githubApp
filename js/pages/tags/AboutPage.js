9372d8aa4c8869e65c66056757a6be86be5ebd7d
notBad <1873313960@qq.com>
2018-09-04 17:34:17 +0800

    我的页面

--- js/pages/tags/AboutPage.js
+++ js/pages/tags/AboutPage.js
@@ -2,3 +2,166 @@
  * Created by Administrator on 2018-9-4.
  * 关于页面
  */
+import React, {Component} from 'react';
+import {
+    Dimensions,
+    Image,
+    PixelRatio,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+import ParallaxScrollView from 'react-native-parallax-scroll-view';
+
+const window = Dimensions.get('window');
+
+const AVATAR_SIZE = 120;
+const ROW_HEIGHT = 60;
+const PARALLAX_HEADER_HEIGHT = 350;
+const STICKY_HEADER_HEIGHT = 70;
+
+export default class AboutPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    getParallaxRenderConfig(params) {
+        let config = {};
+        config.renderBackground = () => (
+            <View key="background">
+                <Image source={{
+                    uri: params.backgroundImage,
+                    width: window.width,
+                    height: PARALLAX_HEADER_HEIGHT
+                }}/>
+                <View style={{
+                    position: 'absolute',
+                    top: 0,
+                    width: window.width,
+                    backgroundColor: 'rgba(0,0,0,.4)',
+                    height: PARALLAX_HEADER_HEIGHT
+                }}/>
+            </View>
+        );
+
+        config.renderForeground = () => (
+            <View key="parallax-header" style={ styles.parallaxHeader }>
+                <Image style={ styles.avatar } source={{
+                    uri: params.avator,
+                    width: AVATAR_SIZE,
+                    height: AVATAR_SIZE
+                }}/>
+                <Text style={ styles.sectionSpeakerText }>
+                    {params.name}
+                </Text>
+                <Text style={ styles.sectionTitleText }>
+                    {params.description}
+                </Text>
+            </View>
+        );
+
+        config.renderStickyHeader = () => (
+            <View key="sticky-header" style={styles.stickySection}>
+                <Text style={styles.stickySectionText}>{params.name}</Text>
+            </View>
+        );
+
+        config.renderFixedHeader = () => (
+            <View key="fixed-header" style={styles.fixedSection}>
+            </View>
+        );
+
+
+        return config;
+    }
+
+    renderView(params) {
+        let renderConfig = this.getParallaxRenderConfig(params);
+        return (
+            <ParallaxScrollView
+                headerBackgroundColor="#333"
+                stickyHeaderHeight={ STICKY_HEADER_HEIGHT }
+                parallaxHeaderHeight={ PARALLAX_HEADER_HEIGHT }
+                backgroundSpeed={10}
+                {...renderConfig}
+            />
+        );
+    }
+
+    render() {
+        return this.renderView({
+            'name': 'GitHub Popular',
+            'description': '这是一个用来查看GitHub最受欢迎与最热项目的APP，它基于react Native支持android和ios双平台',
+            'avator': 'https://avatar.csdn.net/8/6/F/1_notbad_.jpg',
+            'backgroundImage': 'http://t2.hddhhn.com/uploads/tu/201801/9999/c63fb8c291.jpg'
+        })
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black'
+    },
+    background: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: window.width,
+        height: PARALLAX_HEADER_HEIGHT
+    },
+    stickySection: {
+        height: STICKY_HEADER_HEIGHT,
+        width: 300,
+        justifyContent: 'flex-end'
+    },
+    stickySectionText: {
+        color: 'white',
+        fontSize: 20,
+        margin: 10
+    },
+    fixedSection: {
+        position: 'absolute',
+        bottom: 10,
+        right: 10
+    },
+    fixedSectionText: {
+        color: '#999',
+        fontSize: 20
+    },
+    parallaxHeader: {
+        alignItems: 'center',
+        flex: 1,
+        flexDirection: 'column',
+        paddingTop: 100
+    },
+    avatar: {
+        marginBottom: 10,
+        borderRadius: AVATAR_SIZE / 2
+    },
+    sectionSpeakerText: {
+        color: 'white',
+        fontSize: 24,
+        paddingVertical: 5
+    },
+    sectionTitleText: {
+        color: 'white',
+        fontSize: 18,
+        paddingVertical: 5
+    },
+    row: {
+        overflow: 'hidden',
+        paddingHorizontal: 10,
+        height: ROW_HEIGHT,
+        backgroundColor: 'white',
+        borderColor: '#ccc',
+        borderBottomWidth: 1,
+        justifyContent: 'center'
+    },
+    rowText: {
+        fontSize: 20
+    }
+});
